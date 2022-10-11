ae5d992e5c75a52408f8d5613868ee846f3c525b
Vũ Đức Duy <duydvu98@gmail.com>
2022-10-11 23:42:09 +0700

    Implement export

--- src/module.vue
+++ src/module.vue
@@ -1,26 +1,38 @@
 <template>
 	<private-view title="Schema Management">
-		<v-button @click="exportSchema">
-			Export
-		</v-button>
-		<v-list>
-			<v-list-item
-				v-for="col in collections"
-				:key="col.collection"
-				block
-				dense
-				clickable
-			>
-				<v-checkbox v-model="selections" :value="col.collection" :label="col.name" />
-			</v-list-item>
-		</v-list>
+		<div class="schema-management-module-wrapper">
+			<div class="action-buttons">
+				<v-button @click="exportSchema">
+					Export
+				</v-button>
+			</div>
+			<div>
+				<v-checkbox
+					v-model="checkAll"
+					@update:model-value="toggleAll"
+					label="Select all"
+					class="collection-item"
+				/>
+				<div>
+					<v-checkbox
+						v-for="col in collections"
+						:key="col.collection"
+						v-model="selections"
+						:value="col.collection"
+						:label="col.name"
+						block
+						class="collection-item"
+					/>
+				</div>
+			</div>
+		</div>
 	</private-view>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref, watch } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 import { useStores } from '@directus/extensions-sdk';
-import { Collection } from '@directus/shared/types';
+import { Collection, Field, Relation } from '@directus/shared/types';
 
 export default defineComponent({
 	setup() {
@@ -35,25 +47,87 @@ export default defineComponent({
 		const relationsStore = useRelationsStore();
 
 		const collections = computed(() => (collectionsStore.allCollections as Collection[]).filter(c => c.schema !== null));
-		const selections = ref([]);
-		watch(selections, () => console.log(selections));
+		const selections = ref<string[]>([]);
+		const checkAll = ref(false);
 
 		return {
 			collections,
 			selections,
+			checkAll,
 			exportSchema,
+			toggleAll,
 		};
 
 		function exportSchema() {
-			const exportCollections = collections.value.map(({ meta, schema }) => ({
-				meta,
-				schema,
-			}));
+			const exportCollections = collections.value
+				.map(({ collection, meta, schema }) => ({ collection, meta, schema }))
+				.filter(c => selections.value.includes(c.collection));
+
+			const fields = selections.value
+				.map(c => fieldsStore.getFieldsForCollection(c) as Field[])
+				.flat()
+				.map(({ name, ...field }) => {
+					const f: Record<string, any> = field;
+					if (field.meta) {
+						const { id, ...meta } = field.meta;
+						f.meta = meta;
+					}
+					return f;
+				});
+
+			const relations = selections.value
+				.map(c => relationsStore.getRelationsForCollection(c) as Relation[])
+				.flat()
+				.map((rel) => {
+					const r: Record<string, any> = rel;
+					if (rel.meta) {
+						const { id, ...meta } = rel.meta;
+						r.meta = meta;
+					}
+					return r;
+				});
+
 			const schema = {
 				collections: exportCollections,
+				fields,
+				relations,
 			};
-			console.log(JSON.stringify(schema, null, 4));
+
+			const schemaContent = JSON.stringify(schema, null, 4);
+
+			const element = document.createElement('a');
+			element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(schemaContent));
+			element.setAttribute('download', 'schema.json');
+
+			element.style.display = 'none';
+			document.body.appendChild(element);
+
+			element.click();
+
+			document.body.removeChild(element);
+		}
+
+		function toggleAll(checked: boolean) {
+			if (checked) {
+				selections.value = collections.value.map(c => c.collection);
+			} else {
+				selections.value = [];
+			}
 		}
 	}
 });
 </script>
+
+<style lang="scss" scoped>
+.schema-management-module-wrapper {
+	margin-left: var(--content-padding);
+
+	.action-buttons {
+		margin-bottom: 16px;
+	}
+
+	.collection-item {
+		margin-bottom: 8px;
+	}
+}
+</style>
